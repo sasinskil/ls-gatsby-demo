b936f145a90b3ed9ec4a3acaa486409ce3fdfe22
sasinskil <sasinskil@gmail.com>
2020-10-28 22:03:01 +0100

    update button hover animation

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -7,21 +7,36 @@ const Button = styled.button`
   width: 100%;
   max-width: 170px;
   font-family: "Montserrat";
-  background: transparent;
-  color: #ffffff;
-  border: 1px solid #ffffff;
+  background: #ffffff;
+  color: #000000;
+  border: none;
   font-size: 1rem;
   font-weight: 500;
   cursor: pointer;
   outline: none;
   box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
     0 10px 10px -5px rgba(0, 0, 0, 0.04);
-  transition: 0.2s ease-in-out;
-  transition-property: background, color;
+  transition: transform 0.3s ease;
 
   :hover {
-    color: #000000;
-    background: #ffffff;
+    transform: translate(-4px, -4px);
+  }
+
+  :hover::before {
+    transform: translate(4px, 4px);
+  }
+
+  ::before {
+    position: absolute;
+    content: "";
+    display: block;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    border: 1px solid #ffffff;
+    transition: transform 0.3s ease;
   }
 `
 
